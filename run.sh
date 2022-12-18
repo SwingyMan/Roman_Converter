d3a80b269a0d48b34e5b275b4d44a1662b2f2e9a
marcin <asssinowo90@gmail.com>
2022-12-18 20:17:47 +0100

    final fix

--- run.sh
+++ run.sh
@@ -1,20 +1,34 @@
 #!/bin/bash
 date=$(date "+%Y-%m-%d-%H:%M:%S")
-cd in
-count=$(ls -1q *|wc -l)
-cd ..
-mv *.html "backup/"
-touch "$date.html"
-cd in
-for ((i=1;i<=$count;i++)) ; do
-    :
-    input=$(cat $i.txt)
-    cd ..
-    result=$(python3 main.py $input)
-    cd out
-    echo $result > "$i.txt"
-    cd ..
-    cd in
+while choice=$(dialog --title "Kalendarz Rzymski" --menu "Wybierz opcję" 12 45 25 1 "Uruchom skrypt" 2 "Stwórz Backup" 3 "O autorze" 4 "Wyjdz" 2>&1 >/dev/tty)
+  do
+  case $choice in
+      1) cd in
+          count=$(ls -1q *|wc -l)
+          cd ..
+          rm *.html
+          touch "$date.html"
+          cd in
+          for ((i=1;i<=$count;i++)) ; do
+              :
+              input=$(cat $i.txt)
+              cd ..
+              result=$(python3 main.py $input)
+              cd out
+              echo $result > "$i.txt"
+              cd ..
+              cd in
+          done
+          cd ..
+          python3 generate.py "$date.html"
+          dialog --title "Gotowe" --msgbox "Zadanie wykonane!" 6 20;;
+      2) mkdir backup/$date
+          mv *.html "backup/$date/"
+          cp -R in backup/$date/
+          cp -R out backup/$date/
+          dialog --title "Gotowe" --msgbox "Zadanie wykonane!" 6 20;;
+      3)  dialog --title "Gotowe" --msgbox "Projekt stworzony przez Marcina Bogus w językach Python i Bash.
+Celem programu miała być dwustronna zamiana systemów cyfr rzymskich i arabskich." 12 80;;
+      *) exit;;
+  esac
 done
-cd ..
-python3 generate.py "$date.html"
